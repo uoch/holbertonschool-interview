99fcf617e7a7fd65394c00c4e09ec020af659cbf
uoch <chaouch.thameurtc61@gmail.com>
2023-09-14 12:28:11 +0100

    sandpales

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -6,18 +6,18 @@
  */
 void print(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 /**
  * toppel_check - sum of two sandpiles
@@ -27,19 +27,19 @@ void print(int grid[3][3])
  */
 bool toppel_check(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                return true;
-            }
-        }
-    }
-    return false;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
 /**
  * do_toppel - topple the cell to her neighbors
@@ -49,42 +49,42 @@ bool toppel_check(int grid[3][3])
  */
 void do_toppel(int grid[3][3])
 {
-    int i, j;
-    if (toppel_check(grid) == false)
-    {
-        return;
-    }
-    else
-    {
-        printf("=\n");
-        print(grid);
-        while (toppel_check(grid) == true)
-        {
-            for (i = 0; i < 3; i++)
-            {
-                for (j = 0; j < 3; j++)
-                {
+	int i, j;
+	if (toppel_check(grid) == false)
+	{
+		return;
+	}
+	else
+	{
+		printf("=\n");
+		print(grid);
+		while (toppel_check(grid) == true)
+		{
+			for (i = 0; i < 3; i++)
+			{
+				for (j = 0; j < 3; j++)
+				{
 
-                    if (grid[i][j] > 3)
-                    {
-                        grid[i][j] -= 4;
-                        if (i - 1 >= 0)
-                            grid[i - 1][j] += 1;
-                        if (i + 1 < 3)
-                            grid[i + 1][j] += 1;
-                        if (j - 1 >= 0)
-                            grid[i][j - 1] += 1;
-                        if (j + 1 < 3)
-                            grid[i][j + 1] += 1;
-                    }
-                }
-            }
-            if (toppel_check(grid) == false)
-                break;
-            printf("=\n");
-            print(grid);
-        }
-    }
+					if (grid[i][j] > 3)
+					{
+						grid[i][j] -= 4;
+						if (i - 1 >= 0)
+							grid[i - 1][j] += 1;
+						if (i + 1 < 3)
+							grid[i + 1][j] += 1;
+						if (j - 1 >= 0)
+							grid[i][j - 1] += 1;
+						if (j + 1 < 3)
+							grid[i][j + 1] += 1;
+					}
+				}
+			}
+			if (toppel_check(grid) == false)
+				break;
+			printf("=\n");
+			print(grid);
+		}
+	}
 }
 
 /**
@@ -96,24 +96,24 @@ void do_toppel(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    int gridsum[3][3];
+	int i, j;
+	int gridsum[3][3];
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            gridsum[i][j] = grid1[i][j] + grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			gridsum[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] = gridsum[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = gridsum[i][j];
+		}
+	}
 
-    do_toppel(grid1);
+	do_toppel(grid1);
 }
