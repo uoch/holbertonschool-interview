5add1bc049ab1e8b2578c917b8e40ff9e348036a
uoch <chaouch.thameurtc61@gmail.com>
2023-09-14 11:51:01 +0100

    header

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+#include <stdio.h>
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+bool toppel_check(int grid[3][3]);
+void do_toppel(int grid[3][3]);
+void print(int grid[3][3]);
