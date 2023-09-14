0de526d4cf5e022438787953301fb444b6cdcf98
uoch <chaouch.thameurtc61@gmail.com>
2023-09-14 12:37:20 +0100

    sandpales

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,6 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 #include <stdbool.h>
 #include <stdio.h>
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 bool toppel_check(int grid[3][3]);
 void do_toppel(int grid[3][3]);
 void print(int grid[3][3]);
+
+#endif /* SANDPILES_H */
