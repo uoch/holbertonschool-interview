319d4c84ad5b03011bd9fcf48e7245b5bc95be6a
uoch <chaouch.thameurtc61@gmail.com>
2023-09-29 14:06:10 +0100

    12.

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,35 +9,34 @@ status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 def parse(line):
     """Parse a line"""
-    data = line.rstrip().split()
     try:
-        status_code = int(data[-2])
-        file_size = int(data[-1])
+        parts = line.strip().split()
+        status_code = int(parts[-2])
+        file_size = int(parts[-1])
         if status_code not in status:
             return None
         return {"status_code": status_code, "file_size": file_size}
-    except Exception:
-        pass
+    except (ValueError, IndexError):
+        return None
 
 
 def parse10(data_list):
     """parse 10 lines of generated logs"""
     file_size = 0
-    try:
-        for data in data_list:
-            file_size += data["file_size"]
-            status_count[data["status_code"]] += 1
-        return file_size, status_count
-    except Exception:
-        pass
+    for data in data_list:
+        file_size += data["file_size"]
+        status_count[data["status_code"]] += 1
+    return file_size, status_count
+
 
 
 def print_stats(file_size, status_count):
     """print stats"""
-    print("File size: {}".format(file_size))
-    for k, v in sorted(status_count.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    print(f"File size: {file_size}")
+    for code in sorted(status):
+        count = status_count.get(code, 0)
+        if count > 0:
+            print(f"{code}: {count}")
 
 
 if __name__ == "__main__":
@@ -45,18 +44,22 @@ if __name__ == "__main__":
     h = 0
     try:
         for input_line in sys.stdin:
-            if input_line == "":
+            if input_line.strip() == "":
                 print("file size: 0")
-            data_list.append(parse(input_line))
-            if len(data_list) == 10:
-                file_size, status_count = parse10(data_list)
-                h = file_size
-                print_stats(file_size, status_count)
-                data_list = []
+            else:
+                data = parse(input_line)
+                if data:
+                    data_list.append(data)
+                    if len(data_list) == 10:
+                        file_size, status_count = parse10(data_list)
+                        h = file_size
+                        print_stats(file_size, status_count)
+                        data_list = []
         # emptying the buffer
-        x, y = parse10(data_list)
-        x += h
-        print_stats(x, y)
+        if data_list:
+            x, y = parse10(data_list)
+            x += h
+            print_stats(x, y)
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
         exit(0)
