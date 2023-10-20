93b73fce46a3e4b6df0db286eb8bacd9624dea04
uoch <chaouch.thameurtc61@gmail.com>
2023-10-20 14:11:54 +0100

    utf_8

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""validUTF8 module"""
+
+
+def validUTF8(data):
+    # Initialize byte count to zero
+    byte_count = 0
+
+    for num in data:
+        binary_num = bin(num)[2:]
+        binary_num = binary_num.zfill(8)
+
+        if byte_count == 0:
+            if binary_num[0] == '0':
+                byte_count = 0
+            elif binary_num[0] == '110':
+                byte_count = 1
+            elif binary_num[0:3] == '1110':
+                byte_count = 2
+            elif binary_num[0:4] == '11110':
+                byte_count = 3
+            else:
+                return False
+        else:
+            # Check that the byte starts with '10' as a continuation byte
+            if binary_num[0:2] != '10':
+                return False
+            byte_count -= 1
+
+    return byte_count == 0
