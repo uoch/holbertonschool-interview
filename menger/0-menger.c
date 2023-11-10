be9cf6bd81ff6cec2e9834c57fc25db4a63e0c57
uoch <chaouch.thameurtc61@gmail.com>
2023-11-10 11:18:45 +0100

    func

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -6,34 +6,34 @@
  */
 void menger(int level)
 {
-    int i, j, size, x, y;
-    char c;
+	int i, j, size, x, y;
+	char c;
 
-    size = pow(3, level);
-    if (level < 0)
-        return;
-    if (level == 0)
-    {
-        printf("#\n");
-        return;
-    }
+	size = pow(3, level);
+	if (level < 0)
+		return;
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            x = i;
-            y = j;
-            c = '#';
-            while (x > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                    c = ' ';
-                x /= 3;
-                y /= 3;
-            }
-            printf("%c", c);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			x = i;
+			y = j;
+			c = '#';
+			while (x > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+					c = ' ';
+				x /= 3;
+				y /= 3;
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+	}
 }
