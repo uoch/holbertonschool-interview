329d8b437c82de6d25b16fdc6b82c452eb067ae0
uoch <chaouch.thameurtc61@gmail.com>
2023-11-10 11:17:26 +0100

    func

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,27 +1,32 @@
-# include "menger.h"
+#include "menger.h"
 /**
  * menger - draws a 2D Menger Sponge
  * @level: level of the Menger Sponge to draw
  * Return: void
  */
-void menger(int level){
+void menger(int level)
+{
     int i, j, size, x, y;
     char c;
 
     size = pow(3, level);
     if (level < 0)
         return;
-    if (level == 0){
+    if (level == 0)
+    {
         printf("#\n");
         return;
     }
-    
-    for (i = 0; i < size; i++){
-        for (j = 0; j < size; j++){
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
             x = i;
             y = j;
             c = '#';
-            while (x > 0){
+            while (x > 0)
+            {
                 if (x % 3 == 1 && y % 3 == 1)
                     c = ' ';
                 x /= 3;
