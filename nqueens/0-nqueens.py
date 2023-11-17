852b80af0e19ba6849053370a0e47ab26b28a27a
uoch <chaouch.thameurtc61@gmail.com>
2023-11-17 17:09:16 +0100

    red

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -32,7 +32,7 @@ def solve_nqueens(n):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(f"Usage: nqueens{sys}")
+        print(f"Usage: nqueens N")
         sys.exit(1)
 
     try:
