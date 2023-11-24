6ba7b1834ad61c3252991b8864e6e9101c22cf85
uoch <chaouch.thameurtc61@gmail.com>
2023-11-24 15:16:57 +0100

    try

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -4,26 +4,25 @@
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
  * Return: Pointer to the root node of the created AVL tree, or NULL on failure
-
 */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *root;
-    int mid;
+	avl_t *root;
+	int mid;
 
-    if (size == 0)
-        return (NULL);
-    root = malloc(sizeof(avl_t));
-    if (root == NULL)
-        return (NULL);
-    mid = (size - 1) / 2;
-    root->n = array[mid];
-    root->parent = NULL;
-    root->left = sorted_array_to_avl(array, mid);
-    root->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
-    if (root->left != NULL)
-        root->left->parent = root;
-    if (root->right != NULL)
-        root->right->parent = root;
-    return (root);
+	if (size == 0)
+		return (NULL);
+	root = malloc(sizeof(avl_t));
+	if (root == NULL)
+		return (NULL);
+	mid = (size - 1) / 2;
+	root->n = array[mid];
+	root->parent = NULL;
+	root->left = sorted_array_to_avl(array, mid);
+	root->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
+	if (root->left != NULL)
+		root->left->parent = root;
+	if (root->right != NULL)
+		root->right->parent = root;
+	return (root);
 }
--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -24,5 +24,4 @@ typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
-
 #endif
