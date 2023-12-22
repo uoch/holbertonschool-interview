d0c5e48b48b05e4aa7fbc3498cfe8551c388b65d
uoch <chaouch.thameurtc61@gmail.com>
2023-12-22 15:14:27 +0100

    heap

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -9,27 +9,26 @@
  * @size: size of the array
  * @i: i index
  * @total_size: total_size of the array
-
-*/
+ */
 void heapify(int *array, int size, int i, int total_size)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int temp;
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	int temp;
 
-    if (left < size && array[left] > array[largest])
-        largest = left;
-    if (right < size && array[right] > array[largest])
-        largest = right;
-    if (largest != i)
-    {
-        temp = array[i];
-        array[i] = array[largest];
-        array[largest] = temp;
-        print_array(array, total_size);
-        heapify(array, size, largest, total_size);
-    }
+	if (left < size && array[left] > array[largest])
+		largest = left;
+	if (right < size && array[right] > array[largest])
+		largest = right;
+	if (largest != i)
+	{
+		temp = array[i];
+		array[i] = array[largest];
+		array[largest] = temp;
+		print_array(array, total_size);
+		heapify(array, size, largest, total_size);
+	}
 }
 
 /**
@@ -40,18 +39,18 @@ void heapify(int *array, int size, int i, int total_size)
 
 void heap_sort(int *array, size_t size)
 {
-    int i, temp;
+	int i, temp;
 
-    if (array == NULL || size < 2)
-        return;
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, i, size);
-    for (i = size - 1; i > 0; i--)
-    {
-        temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
-        print_array(array, size);
-        heapify(array, i, 0, size);
-    }
+	if (array == NULL || size < 2)
+		return;
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i > 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		heapify(array, i, 0, size);
+	}
 }
