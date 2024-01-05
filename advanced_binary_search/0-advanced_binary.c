606d18fe3891546e6be1c7c5f0597d3dac55d613
uoch <chaouch.thameurtc61@gmail.com>
2024-01-05 14:50:34 +0100

    try

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -31,6 +31,13 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 	}
 	return (-1);
 }
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value or -1 if not found
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
