c99bf3bcf3a51749435ab6b880170b8a34fd47f1
uoch <chaouch.thameurtc61@gmail.com>
2024-01-19 15:59:28 +0100

    try

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -1,31 +1,41 @@
 #include "binary_trees.h"
 
+/**
+ * heap_extract - extracts the root value of a Max Binary Heap
+ * @root: double pointer to the root node of the heap
+ *
+ * Return: the extracted value, or 0 if the heap is empty
+ */
+int heap_extract(heap_t **root)
+{
+	heap_t *node = NULL;
+	int value = 0;
 
-int heap_extract(heap_t **root){
-    heap_t *node = NULL;
-    int value = 0;
+	if (!root || !*root)
+		return (0);
 
-    if (!root || !*root)
-        return (0);
-    node = *root;
-    value = node->n;
-    while (node->left || node->right)
-    {
-        if (!node->right || node->left->n > node->right->n)
-        {
-            node->n = node->left->n;
-            node = node->left;
-        }
-        else if (!node->left || node->left->n < node->right->n)
-        {
-            node->n = node->right->n;
-            node = node->right;
-        }
-    }
-    if (node->parent->left == node)
-        node->parent->left = NULL;
-    else
-        node->parent->right = NULL;
-    free(node);
-    return (value);
+	node = *root;
+	value = node->n;
+
+	while (node->left || node->right)
+	{
+		if (!node->right || node->left->n > node->right->n)
+		{
+			node->n = node->left->n;
+			node = node->left;
+		}
+		else if (!node->left || node->left->n < node->right->n)
+		{
+			node->n = node->right->n;
+			node = node->right;
+		}
+	}
+
+	if (node->parent->left == node)
+		node->parent->left = NULL;
+	else
+		node->parent->right = NULL;
+
+	free(node);
+	return (value);
 }
--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -13,10 +13,10 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
