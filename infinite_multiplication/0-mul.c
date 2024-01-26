0f7dec50b80eee4f4154dd740e989dd703bbb589
uoch <chaouch.thameurtc61@gmail.com>
2024-01-26 07:20:55 +0100

    try

---
+++ infinite_multiplication/0-mul.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n / 10 != 0)
+        print_number(n / 10);
+
+    _putchar(n % 10 + '0');
+}
+
+/**
+ * is_digit_str - checks if a string contains only digits
+ * @str: input string
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit_str(char *str)
+{
+    while (*str)
+    {
+        if (*str < '0' || *str > '9')
+            return 0;
+        str++;
+    }
+    return 1;
+}
+
+/**
+ * exit_error - prints "Error" and exits with status 98
+ */
+void exit_error(void)
+{
+    char error[] = "Error\n";
+    int i = 0;
+    while (error[i])
+    {
+        _putchar(error[i]);
+        i++;
+    }
+    exit(98);
+}
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+int main(int argc, char *argv[])
+{
+    int a, b, product;
+
+    if (argc != 3 || !is_digit_str(argv[1]) || !is_digit_str(argv[2]))
+    {
+        exit_error();
+    }
+
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    product = mul(a, b);
+
+    if (product < 0)
+    {
+        _putchar('-');
+        product = -product;
+    }
+
+    if (product == 0)
+    {
+        _putchar('0');
+    }
+    else
+    {
+        print_number(product);
+    }
+
+    _putchar('\n');
+
+    return 0;
+}
---
+++ infinite_multiplication/mul.h
@@ -0,0 +1,8 @@
+#ifndef _MUL_H_
+#define _MUL_H_
+
+int _putchar(char c);
+
+
+
+#endif
