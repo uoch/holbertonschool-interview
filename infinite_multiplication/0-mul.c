1f9ed44f5c892170b3784b6a68b61cfc8fed4e90
uoch <chaouch.thameurtc61@gmail.com>
2024-01-26 08:35:38 +0100

    school_mull

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -25,17 +25,66 @@ void print_number(long long n)
  * is_digit_str - checks if a string contains only digits
  * @str: input string
  *
- * Return: 1 if all characters are digits, 0 otherwise
+ * Return: an array of integers representing the digits of the string
  */
-int is_digit_str(char *str)
+int *is_digit_str(char *str)
 {
-	while (*str)
+	int i, *arr;
+
+	i = 0;
+	while (str[i])
 	{
-		if (*str < '0' || *str > '9')
-			return 0;
-		str++;
+		if (str[i] < '0' || str[i] > '9')
+			return NULL;
+		i++;
 	}
-	return 1;
+
+	arr = malloc(sizeof(int) * i);
+	if (arr == NULL)
+		return NULL;
+
+	while (i--)
+	{
+		arr[i] = str[i] - '0';
+	}
+
+	return arr;
+}
+
+/**
+ * arr_mul - multiplies two arrays of integers
+ * @arr1: first array
+ * @arr2: second array
+ * @len1: length of the first array
+ * @len2: length of the second array
+ *
+ * Return: an array of integers representing the result of multiplication
+ */
+int *arr_mul(int *arr1, int *arr2, int len1, int len2)
+{
+	int i, j, k, len;
+	int *result;
+
+	len = len1 + len2;
+	result = malloc(sizeof(int) * len);
+	if (result == NULL)
+		return NULL;
+
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			k = len1 + len2 - 2 - i - j;
+			result[k] += arr1[i] * arr2[j];
+			result[k + 1] += result[k] / 10;
+			result[k] %= 10;
+		}
+	}
+
+	return result;
 }
 
 /**
@@ -51,46 +100,6 @@ void exit_error(void)
 		i++;
 	}
 	exit(98);
-}
-
-/**
- * school_mul - performs multiplication of two large numbers
- * @arr1: array representing the first number
- * @arr2: array representing the second number
- * @len1: length of the first array
- * @len2: length of the second array
- *
- * Return: 0 on success
- */
-int school_mul(char *arr1, char *arr2, int len1, int len2)
-{
-	int i, j, k = 0, carry = 0, sum = 0;
-	int *result = malloc(sizeof(int) * (len1 + len2));
-
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = 0;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			sum = (arr1[i] - '0') * (arr2[j] - '0') + result[i + j + 1] + carry;
-			carry = sum / 10;
-			result[i + j + 1] = sum % 10;
-		}
-		result[i + j + 1] = carry;
-	}
-
-	while (result[k] == 0)
-		k++;
-
-	for (i = k; i < len1 + len2; i++)
-		_putchar(result[i] + '0');
-
-	_putchar('\n');
-	free(result);
-	return (0);
 }
 
 /**
@@ -113,7 +122,25 @@ int main(int argc, char *argv[])
 	while (argv[2][len2])
 		len2++;
 
-	school_mul(argv[1], argv[2], len1, len2);
+	int *num1 = is_digit_str(argv[1]);
+	int *num2 = is_digit_str(argv[2]);
 
-	return (0);
+	if (num1 == NULL || num2 == NULL)
+		exit_error();
+
+	int *result = arr_mul(num1, num2, len1, len2);
+
+	if (result == NULL)
+		exit_error();
+
+	for (int i = 0; i < len1 + len2; i++)
+		_putchar(result[i] + '0');
+
+	_putchar('\n');
+
+	free(num1);
+	free(num2);
+	free(result);
+
+	return 0;
 }
