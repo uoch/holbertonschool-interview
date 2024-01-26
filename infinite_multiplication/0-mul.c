5fe98f068abc10462ced6624298f364aebe5ed96
uoch <chaouch.thameurtc61@gmail.com>
2024-01-26 09:53:02 +0100

    school_mull

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,97 +1,145 @@
-#include"holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int str_len(char *str)
+int _putchar(char c);
+
+/**
+ * print_number - prints a long long integer
+ * @n: long long integer to be printed
+ */
+void print_number(long long n)
 {
-    int i = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    for (i = 0; str[i] != '\0'; i++)
-        ;
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-    return (i);
+	_putchar(n % 10 + '0');
 }
 
-void print_error(void)
+/**
+ * is_digit_str - checks if a string contains only digits
+ * @str: input string
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit_str(char *str)
 {
-    _putchar('E');
-    _putchar('r');
-    _putchar('r');
-    _putchar('o');
-    _putchar('r');
-    _putchar('\n');
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return 0;
+		str++;
+	}
+
+	return 1;
 }
 
-int is_digit(char *str)
+/**
+ * arr_mul - multiplies two arrays of integers
+ * @arr1: first array
+ * @arr2: second array
+ * @len1: length of the first array
+ * @len2: length of the second array
+ * @result: array to store the result
+ */
+void arr_mul(int *arr1, int *arr2, int len1, int len2, int *result)
 {
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return 0;
-        str++;
-    }
+	int i, j;
 
-    return 1;
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			result[i + j + 1] += arr1[i] * arr2[j];
+			result[i + j] += result[i + j + 1] / 10;
+			result[i + j + 1] %= 10;
+		}
+	}
 }
 
+/**
+ * exit_error - prints "Error" and exits with status 98
+ */
+void exit_error(void)
+{
+	char error[] = "Error\n";
+	int i = 0;
+
+	while (error[i])
+	{
+		_putchar(error[i]);
+		i++;
+	}
+
+	exit(98);
+}
+
+/**
+ * main - entry point of the program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-    int len1, len2, i;
-    int *num1, *num2, *result;
+	int len1, len2, i;
+	int *num1, *num2, *result;
 
-    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-    {
-        print_error();
-        exit(98);
-    }
+	if (argc != 3 || !is_digit_str(argv[1]) || !is_digit_str(argv[2]))
+		exit_error();
 
-    len1 = str_len(argv[1]);
-    len2 = str_len(argv[2]);
+	len1 = 0;
+	len2 = 0;
 
-    num1 = malloc(sizeof(int) * len1);
-    num2 = malloc(sizeof(int) * len2);
+	while (argv[1][len1])
+		len1++;
 
-    if (num1 == NULL || num2 == NULL)
-    {
-        print_error();
-        exit(98);
-    }
+	while (argv[2][len2])
+		len2++;
 
-    for (i = 0; i < len1; i++)
-        num1[i] = argv[1][i] - '0';
+	num1 = is_digit_str(argv[1]) ? malloc(sizeof(int) * len1) : NULL;
+	num2 = is_digit_str(argv[2]) ? malloc(sizeof(int) * len2) : NULL;
 
-    for (i = 0; i < len2; i++)
-        num2[i] = argv[2][i] - '0';
+	if (num1 == NULL || num2 == NULL)
+		exit_error();
 
-    result = malloc(sizeof(int) * (len1 + len2));
-    if (result == NULL)
-    {
-        print_error();
-        exit(98);
-    }
+	for (i = 0; i < len1; i++)
+		num1[i] = argv[1][i] - '0';
 
-    /* Multiplication logic */
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = 0;
+	for (i = 0; i < len2; i++)
+		num2[i] = argv[2][i] - '0';
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        for (int j = len2 - 1; j >= 0; j--)
-        {
-            result[i + j + 1] += num1[i] * num2[j];
-            result[i + j] += result[i + j + 1] / 10;
-            result[i + j + 1] %= 10;
-        }
-    }
+	result = malloc(sizeof(int) * (len1 + len2));
+	if (result == NULL)
+		exit_error();
 
-    /* Print the result */
-    for (i = 0; i < len1 + len2; i++)
-    {
-        _putchar(result[i] + '0');
-    }
-    _putchar('\n');
+	arr_mul(num1, num2, len1, len2, result);
 
-    free(num1);
-    free(num2);
-    free(result);
+	// Print the result in the correct order
+	i = 0;
+	while (i < len1 + len2 && result[i] == 0)
+		i++;
 
-    return 0;
+	if (i == len1 + len2)
+		_putchar('0');
+	else
+		for (; i < len1 + len2; i++)
+			_putchar(result[i] + '0');
+
+	_putchar('\n');
+
+	free(num1);
+	free(num2);
+	free(result);
+
+	return 0;
 }
