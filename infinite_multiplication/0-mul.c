1a2bf4f161d107953fd668e27a0a1ff00e1f6a92
uoch <chaouch.thameurtc61@gmail.com>
2024-01-26 07:24:07 +0100

    try

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -4,21 +4,21 @@
 int _putchar(char c);
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints a long long integer
+ * @n: long long integer to be printed
  */
-void print_number(int n)
+void print_number(long long n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n / 10 != 0)
-        print_number(n / 10);
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-    _putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
 
 /**
@@ -28,13 +28,13 @@ void print_number(int n)
  */
 int is_digit_str(char *str)
 {
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return 0;
-        str++;
-    }
-    return 1;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return 0;
+		str++;
+	}
+	return 1;
 }
 
 /**
@@ -42,50 +42,50 @@ int is_digit_str(char *str)
  */
 void exit_error(void)
 {
-    char error[] = "Error\n";
-    int i = 0;
-    while (error[i])
-    {
-        _putchar(error[i]);
-        i++;
-    }
-    exit(98);
+	char error[] = "Error\n";
+	int i = 0;
+	while (error[i])
+	{
+		_putchar(error[i]);
+		i++;
+	}
+	exit(98);
 }
 
-int mul(int a, int b)
+long long mul(long long a, long long b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 int main(int argc, char *argv[])
 {
-    int a, b, product;
+	long long a, b, product;
 
-    if (argc != 3 || !is_digit_str(argv[1]) || !is_digit_str(argv[2]))
-    {
-        exit_error();
-    }
+	if (argc != 3 || !is_digit_str(argv[1]) || !is_digit_str(argv[2]))
+	{
+		exit_error();
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
-    product = mul(a, b);
+	a = atoll(argv[1]);
+	b = atoll(argv[2]);
+	product = mul(a, b);
 
-    if (product < 0)
-    {
-        _putchar('-');
-        product = -product;
-    }
+	if (product < 0)
+	{
+		_putchar('-');
+		product = -product;
+	}
 
-    if (product == 0)
-    {
-        _putchar('0');
-    }
-    else
-    {
-        print_number(product);
-    }
+	if (product == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		print_number(product);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 
-    return 0;
+	return 0;
 }
