cdc1880e37b7dc97a8a942e4964f460d951e7bde
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 09:59:32 +0100

    big_O

--- merge_sort/print_array.c
+++ merge_sort/print_array.c
@@ -1,5 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 /**
  * merge - Merges two subarrays of an array
  *
@@ -45,24 +66,4 @@ void merge(int *array, int *left, size_t left_size, int *right, size_t right_siz
     }
     printf("[Done]: ");
     print_array(array, left_size + right_size);
-}
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
 }
