528710f4b0cb5eabafcc2cd43f2749160f4a13d0
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 10:11:27 +0100

    big_O

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,6 +1,54 @@
 #include "sort.h"
 
-void merge(int *array, int *left, size_t left_size, int *right, size_t right_size);
+
+
+/**
+ * merge - Merges two subarrays of an array
+ *
+ * @array: The array to be sorted
+ * @left: The left subarray
+ * @left_size: Number of elements in @left
+ * @right: The right subarray
+ * @right_size: Number of elements in @right
+ */
+void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
+{
+    size_t i = 0, j = 0, k = 0;
+    printf("Merging...\n");
+    printf("[left]: ");
+    print_array(left, left_size);
+    printf("[right]: ");
+    print_array(right, right_size);
+    while (i < left_size && j < right_size)
+    {
+        if (left[i] < right[j])
+        {
+            array[k] = left[i];
+            i++;
+        }
+        else
+        {
+            array[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < left_size)
+    {
+        array[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < right_size)
+    {
+        array[k] = right[j];
+        j++;
+        k++;
+    }
+    printf("[Done]: ");
+    print_array(array, left_size + right_size);
+}
+
 /**
  * merge_sort - Sorts an array of integers in ascending order
  * @array: The array to be sorted
