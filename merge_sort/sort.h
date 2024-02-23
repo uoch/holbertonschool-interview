f25a38e0afb009807c4b50be3dd1a5017b5d86da
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 11:23:31 +0100

    sort

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-void merge(int *array, int *left, size_t left_size, int *right, size_t right_size);
+void merge(int *array, int *left,
+           size_t left_size, int *right, size_t right_size);
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-
 
 #endif
