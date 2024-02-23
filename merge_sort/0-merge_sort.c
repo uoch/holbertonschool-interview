c9d3070b989fc3be534951b8a79634c5c371091c
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 10:41:26 +0100

    fix

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 void rec_merge_sort(int *array, int *temp, size_t start, size_t end);
@@ -6,58 +5,77 @@ void rec_merge_sort(int *array, int *temp, size_t start, size_t end);
 void merge(int *array, int *temp, size_t start, size_t mid, size_t end);
 
 void print_array(const int *array, size_t size);
-
+/**
+ * merge_sort - Sorts an array of integers in ascending order using the Merge
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
 void merge_sort(int *array, size_t size)
 {
-    int *temp;
-    if (size < 2)
-        return;
-    temp = malloc(size * sizeof(int));
-    if (temp == NULL)
-        return;
-    rec_merge_sort(array, temp, 0, size - 1);
-    free(temp);
-}
+	int *temp;
 
+	if (size < 2)
+		return;
+	temp = malloc(size * sizeof(int));
+	if (temp == NULL)
+		return;
+	rec_merge_sort(array, temp, 0, size - 1);
+	free(temp);
+}
+/**
+ * rec_merge_sort - Recursively sorts an array of integers in ascending order
+ * @array: The array to be sorted
+ * @temp: A temporary array to store the sorted subarray
+ * @start: The starting index of the subarray
+ * @end: The ending index of the subarray
+ */
 void rec_merge_sort(int *array, int *temp, size_t start, size_t end)
 {
-    size_t mid;
-    if (start >= end)
-        return;
+	size_t mid;
 
-    mid = start + ((end - start) / 2);
-    rec_merge_sort(array, temp, start, mid);
-    rec_merge_sort(array, temp, mid + 1, end);
-    merge(array, temp, start, mid, end);
+	if (start >= end)
+		return;
+
+	mid = start + ((end - start) / 2);
+	rec_merge_sort(array, temp, start, mid);
+	rec_merge_sort(array, temp, mid + 1, end);
+	merge(array, temp, start, mid, end);
 }
-
+/**
+ * merge - Merges two subarrays of an array
+ * @array: The array to be sorted
+ * @temp: A temporary array to store the sorted subarray
+ * @start: The starting index of the subarray
+ * @mid: The middle index of the subarray
+ * @end: The ending index of the subarray
+ */
 void merge(int *array, int *temp, size_t start, size_t mid, size_t end)
 {
-    size_t i = start, j = mid + 1, k = start;
+	size_t i = start, j = mid + 1, k = start;
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(array + start, mid - start + 1);
-    printf("[right]: ");
-    print_array(array + mid + 1, end - mid);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + start, mid - start + 1);
+	printf("[right]: ");
+	print_array(array + mid + 1, end - mid);
 
-    while (i <= mid && j <= end)
-    {
-        if (array[i] <= array[j])
-            temp[k++] = array[i++];
-        else
-            temp[k++] = array[j++];
-    }
+	while (i <= mid && j <= end)
+	{
+		if (array[i] <= array[j])
+			temp[k++] = array[i++];
+		else
+			temp[k++] = array[j++];
+	}
 
-    while (i <= mid)
-        temp[k++] = array[i++];
+	while (i <= mid)
+		temp[k++] = array[i++];
 
-    while (j <= end)
-        temp[k++] = array[j++];
+	while (j <= end)
+		temp[k++] = array[j++];
 
-    for (i = start; i <= end; i++)
-        array[i] = temp[i];
+	for (i = start; i <= end; i++)
+		array[i] = temp[i];
 
-    printf("[Done]: ");
-    print_array(array + start, end - start + 1);
+	printf("[Done]: ");
+	print_array(array + start, end - start + 1);
 }
