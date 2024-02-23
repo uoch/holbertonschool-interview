032f9df5a1678f76de5673bb45ba546b15e290ae
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 09:45:10 +0100

    big_O

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -6,6 +6,7 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *array, int *left, size_t left_size, int *right, size_t right_size);
 
 
 
