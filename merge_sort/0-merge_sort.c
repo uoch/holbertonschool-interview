78fc7c58d1e0d89f7c19b8cffaa7e6f215092c58
uoch <chaouch.thameurtc61@gmail.com>
2024-02-23 10:06:56 +0100

    big_O

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-
+void merge(int *array, int *left, size_t left_size, int *right, size_t right_size);
 /**
  * merge_sort - Sorts an array of integers in ascending order
  * @array: The array to be sorted
