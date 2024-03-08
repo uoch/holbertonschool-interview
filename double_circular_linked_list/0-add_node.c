15fc09488a8730737ff7320de6581d93dabdf416
chaouch <chaouch.thameurtc61@gmail.com>
2024-03-08 09:03:57 +0100

    list

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,66 @@
+#include "list.h"
+
+/**
+ * add_node_begin - Add a new node to the beg of a double circ linked list
+ *
+ * @list: A pointer to the head of the linkd list
+ * @str: The string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+
+List *add_node_end(List **list, char *str)
+{
+	List *new_node = malloc(sizeof(List));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = str;
+	if (*list == NULL)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		new_node->next = *list;
+		new_node->prev = (*list)->prev;
+		(*list)->prev->next = new_node;
+		(*list)->prev = new_node;
+	}
+	return (new_node);
+}
+/**
+ * add_node_begin - Add a new node to the start of a double circul linked list
+ * @list: A pointer to the head of the linkd list
+ * @str: The string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node = malloc(sizeof(List));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = str;
+	if (*list == NULL)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		new_node->next = *list;
+		new_node->prev = (*list)->prev;
+		(*list)->prev->next = new_node;
+		(*list)->prev = new_node;
+		*list = new_node;
+	}
+	return (new_node);
+}
